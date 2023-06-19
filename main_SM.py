a7d760616b6c0cc2212bc34644dfb0903d63f5d8
Maroof <Shazmeen.Maroof@ons.gov.uk>
2023-06-19 09:57:12 +0100

    adding for loop for showing the correct position in the guess

--- main_SM.py
+++ main_SM.py
@@ -8,13 +8,17 @@ for word in word_list:
     answer = random.choice(word_list)
     print(answer)
     
-# Allowing users to randomly choose a 5 letters long word in English within maximum 6 guesses
+# Allowing users to input a 5 letters long word in English, guess the correct word in maximum 6 attempts and telling them if the position of the letter in their entry is correct
 for guess in range(1,7):
     guess = input("Input a 5 letters long word in English: ").lower()
     if guess == answer.lower():
         print("Correct")
         break
     else:
-        print("Wrong")
+        guess_str = str(guess)
+        for i, val in enumerate(str(answer)):
+            if guess_str[i] == val:
+                print("The position num {} is correct". format(i+1))
+        print("Your guess is incorrect")
 
 
