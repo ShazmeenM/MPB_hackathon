86469b5a6cf1a35566367b1ac6228b61946de1db
Maroof <Shazmeen.Maroof@ons.gov.uk>
2023-06-23 00:18:40 +0100

    adding color coding using for loop

--- main_SM.py
+++ main_SM.py
@@ -1,4 +1,9 @@
+import itertools
 import random
+from colorama import Fore 
+import termcolor
+from termcolor import colored, cprint
+colors = dict(Fore.__dict__.items()) # gets defined colors 
 
 # Creating the word list
 word_list = ['Alert',  'Argue', 'Beach' , 'Begin' , 'Crash' , 'Crime' , 'Drink' , 'Drive' , 'Earth' , 'Empty' , 'Focus' , 'Force', 'Grass' , 'Green' , 'Heart' , 'Hotel' , 'Ideal' , 'Image', 'Juice' , 'Joint' , 'Known' , 'Knead', 'Large' , 'Light']
@@ -6,19 +11,32 @@ word_list = ['Alert',  'Argue', 'Beach' , 'Begin' , 'Crash' , 'Crime' , 'Drink' 
 # Guessing a random word from the list and saving the answer
 for word in word_list:
     answer = random.choice(word_list)
-    print(answer)
-    
+    print(answer.lower())
+
+
 # Allowing users to input a 5 letters long word in English, guess the correct word in maximum 6 attempts and telling them if the position of the letter in their entry is correct
+# Color coding: Black (letter not in chosen word) Green (Letter in the word and in the correct position) Yellow (Letter in the word but not in correct position)
+
 for guess in range(1,7):
     guess = input("Input a 5 letters long word in English: ").lower()
+    position = 0
     if guess == answer.lower():
-        print("Correct")
+        print("Congratulations, you guessed the correct answer.")
         break
-    else:
-        guess_str = str(guess)
-        for i, val in enumerate(str(answer)):
-            if guess_str[i] == val:
-                print("The position num {} is correct". format(i+1))
-        print("Your guess is incorrect")
+    for i, char in enumerate(guess):
+        if answer[i] != guess[i]:
+            print("Error! The word must be 5 letters long.")
+        elif answer[i] == guess[i]:
+            print(guess.replace(guess[i], termcolor.colored(guess[i], 'green')))
+            print("Wrong answer but green letter(s) is in the word and in the correct position.")
+        elif char in answer:
+            print(guess.replace(guess[i], termcolor.colored(guess[i], 'yellow')))
+            print("Wrong answer but yellow letter(s) is in the word.")
+        else:
+            if char not in answer:
+                print(guess)
+                print("Wrong answer. None of the letters are in the word.")
+    
+                
 
 
