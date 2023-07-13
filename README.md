3cb583422f16f24aaeeda62813c31a46aea44f75
Maroof <Shazmeen.Maroof@ons.gov.uk>
2023-07-13 10:46:49 +0100

    replaced the for loop with a function to play Wordle game

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # MPB_hackathon
-Modelling Best Practice Hackathon 
+Modelling Best Practice Hackathon. This pipeline creates a version of the Wordle game.  
 
-The pipeline creates a version of the Wordle game. The Wordle game should (upon running it in Python):
+# How to play Wordle?
 
-1. Randomly choose a 5 letters long word (English).   
-2. Allow a user to enter a real word 5 letters long (words must be checked to make sure they are real English words).  
-3. Returns whether each character entered for that word is:  
-Black (letter not in chosen word)  
-Green (letter in the word and in the correct position)  
-Yellow (letter in the word but not in the correct position)  
-4. Allow users to have a maximum of 6 guesses  
-5. Exit the game if the user gets it correct   
-6. Exit the game if maximum guesses have been reached
+1. Play the game by running main.py.  
+2. The code will ask you to input a 5 letters long word in English.  
+3. You will have 6 tries to guess the word.  
+4. The game will exit if you have guessed the correct word.  
+5. If you guess the exact position of a letter in the answer it will turn GREEN.  
+6. If you guess a letter, but it is in the wrong position it turns YELLOW.  
+7. If the letter does not exist, it will remain black ink.  
+8. The game will exit when maximum guesses have been reached.
+
+# Software requirements
+Python, Virtual environment, for example, Jupyter Notebook or VSCode. 
--- main_SM.py
+++ main_SM.py
@@ -13,45 +13,44 @@ for word in word_list:
     answer = answer.lower()
     print(answer)
 
-# The nested loop below will allow users to input a 5 letters long word in English, guess the correct word in maximum 6 attempts and tell them if they have 1) guessed the correct word, 2) guessed the correct letter in the right place, 3) guess the correct letter in the wrong place.
-# Color coding is used to give clues i.e., Black (letter not in the chosen word), Green (Letter is in the word and in the correct position) and Yellow (Letter is in the word but at a different position)
-
-for guess in range(1,7):
-    guess = input("Input a 5 letters long word in English: ").lower()
+def process_guess():
+    """Function to process the guess in the Wordle game. 
+    
+    Input:
+        input (word): 5 letters long word in English.
+    
+    Returns:
+        Letter in black color: if the letter is not in the chosen word
+        Letter in green color: letter is in the chosen word and in the correct position
+        Letter in yellow color: letter is in the word but at a different position
+        
+    Raises:
+        TypeError: if the guess includes a number or special charater.
+        ValueError: if no guess was entered.
+        ValueError: if the guess contains more than 5 letters.
+    """              
+      
+    allowed_guesses = 6
     clue = ""
-    if guess == answer.lower():
-        print("Congratulations, you guessed the correct answer.")
-        break
-    for i, char in enumerate(guess):
-        if answer[i] == guess[i]:    
-            clue = colored(guess[i], 'green')
-            print(clue, "Correct letter in the right place")
-        elif char in answer:
-            clue = colored(guess[i], 'yellow')
-            print(clue, "Correct letter in the wrong place")
-        elif char not in answer:
-            clue = colored(guess[i], 'black')
-            print(clue, "Incorrect letter")
-                
-
-# Trying to combine nested loops in a function
-
-def main():
-    for guess in range(1,7):
+    while allowed_guesses>0:
         guess = input("Input a 5 letters long word in English: ").lower()
-        clue = ""
         if guess == answer.lower():
             print("Congratulations, you guessed the correct answer.")
             break
-        for i, char in enumerate(guess):
-            if answer[i] == guess[i]:
-                clue = colored(guess[i], 'green')
-                print(clue, "Correct letter in the right place")
-            elif char in answer:
-                clue = colored(guess[i], 'yellow')
-                print(clue, "Correct letter in the wrong place")
-            elif char not in answer:
-                clue = colored(guess[i], 'black')
-                print(clue, "Incorrect letter")
+        else:
+            allowed_guesses = allowed_guesses - 1
+            for i, char in enumerate(guess):
+                if answer[i] == guess[i]:
+                    clue = colored(guess[i], 'green')
+                    print(clue, "Correct letter in the right place")
+                elif char in answer:
+                    clue = colored(guess[i], 'yellow')
+                    print(clue, "Correct letter in the wrong place")
+                elif char not in answer:
+                    clue = colored(guess[i], 'black')
+                    print(clue, "Incorrect letter")
+            if allowed_guesses == 0:
+                print(" You have used maximum allowed guesses, game over!")
                 
+output = process_guess()
 
